113dcd87528692047cb62b85174f88abd8d1f682
aminnicevic04 <aminnicevic@gmail.com>
2023-12-04 19:18:11 +0100

    update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment/moment";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
-function News() {
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+function News({ simplified }) {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "CryptoCurrencies",
+    count: simplified ? 10 : 100,
+  });
+  console.log(cryptoNews);
+
   return <div>News</div>;
 }
 
